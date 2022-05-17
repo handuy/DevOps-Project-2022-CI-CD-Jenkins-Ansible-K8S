70cbf1fcae8e5174c8c05b35b40d13a94a380c97
handuy <handuy1992@gmail.com>
2022-05-17 13:14:22 +0700

    Update jenkins-setup.sh

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install -y java-openjdk11
@@ -9,3 +8,5 @@ sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo yum install -y git
+
+docker run -d -v jenkins_home:/var/jenkins_home -p 10001:8080 -p 50000:50000 --restart always jenkins/jenkins:lts-jdk11
